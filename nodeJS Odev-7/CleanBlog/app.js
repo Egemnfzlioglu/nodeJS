79125fce579be3c1ed886c7ced0134fc865b67cb
Egemen Fazlioglu <egemnfzlioglu@gmail.com>
2022-09-27 22:10:19 +0300

    Clean Blog Proje - 2. Bölüm - Ödev 8

--- nodeJS Odev-7/CleanBlog/app.js
+++ nodeJS Odev-7/CleanBlog/app.js
@@ -1,19 +1,30 @@
 const express = require("express")
+const ejs = require("ejs")
 
 const app = express()
-const PORT = 3000
 
+// Template Engine
+app.set("view engine", "ejs")
+
+// Middlewares
+app.use(express.static("public"))
+
+// Routes
 app.get("/", (req, res) => {
-    const blog = {
-        id: 1,
-        title: "Blog title",
-        description: "Blog description"
-    }
-    //res.json(blog)
-    res.send(blog)
-
+    res.render("index")
+})
+app.get("/about", (req, res) => {
+    res.render("about")
+})
+app.get("/post", (req, res) => {
+    res.render("post")
+})
+app.get("/add_post", (req, res) => {
+    res.render("add_post")
 })
 
+
+const PORT = 4000
 
 app.listen(PORT, () => {
     console.log(`Sunucu ${PORT} Portunda Başladı...`);
