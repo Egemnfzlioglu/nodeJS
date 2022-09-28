da3b378b4498e0dc1721a641b2c6fae2f37eb7d0
Egemen Fazlioglu <egemnfzlioglu@gmail.com>
2022-09-28 04:05:41 +0300

    Clean Blog Proje - 3. Bölüm - Ödev 9

--- nodeJS Odev-7/CleanBlog/app.js
+++ nodeJS Odev-7/CleanBlog/app.js
@@ -1,27 +1,53 @@
 const express = require("express")
+const mongoose = require("mongoose")
+
 const ejs = require("ejs")
+const Post = require("./models/Post")
 
 const app = express()
+
+// connect DB
+mongoose.connect("mongodb://localhost/cleanblog-test-db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 // Template Engine
 app.set("view engine", "ejs")
 
 // Middlewares
 app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))// urldekı datayı okumayı saglıyor
+app.use(express.json())//datayı json a donduruyor
 
-// Routes
-app.get("/", (req, res) => {
-    res.render("index")
+//ROUTES
+
+app.get("/", async (req, res) => {
+    const posts = await Post.find({})
+    res.render("index",
+        {
+            posts
+        })
 })
+
 app.get("/about", (req, res) => {
     res.render("about")
 })
-app.get("/post", (req, res) => {
-    res.render("post")
+
+app.post("/add_post", async (req, res) => {
+    await Post.create(req.body)
+    res.redirect("/")
 })
+
 app.get("/add_post", (req, res) => {
     res.render("add_post")
 })
+
+app.get("/post", (req, res) => {
+    res.render("post")
+})
+
+
 
 
 const PORT = 4000
