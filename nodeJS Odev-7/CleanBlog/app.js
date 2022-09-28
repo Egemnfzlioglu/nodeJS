6706e249600496d5754c9bb827277038d94fbe9c
Egemen Fazlioglu <egemnfzlioglu@gmail.com>
2022-09-28 23:48:30 +0300

    Clean Blog Proje - 5. Bölüm - Ödev 11

--- nodeJS Odev-7/CleanBlog/app.js
+++ nodeJS Odev-7/CleanBlog/app.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const methodOverride = require('method-override')
 
 const ejs = require("ejs")
 const Post = require("./models/Post")
+const pageController = require("./controller/pageController")
+const postController = require("./controller/postController")
 
 const app = express()
 
@@ -16,45 +19,26 @@ mongoose.connect("mongodb://localhost/cleanblog-test-db", {
 app.set("view engine", "ejs")
 
 // Middlewares
-app.use(express.static("public"))
-app.use(express.urlencoded({ extended: true }))// urldekı datayı okumayı saglıyor
-app.use(express.json())//datayı json a donduruyor
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true })) // urldekı datayı okumayı saglıyor
+app.use(express.json())// datayı json a donduruyor 
+app.use(methodOverride('_method', {
+    methods: ["POST", "GET"]
+}))
 
 //ROUTES
 
-app.get("/", async (req, res) => {
-    const posts = await Post.find({})
-    res.render("index",
-        {
-            posts
-        })
-})
-
-app.get("/posts/:id", async (req, res) => {
-    const post = await Post.findById(req.params.id)
-
-    res.render("post",
-        {
-            post
-        })
-})
-
-app.get("/about", (req, res) => {
-    res.render("about")
-})
-
-app.get("/add_post", (req, res) => {
-    res.render("add_post")
-})
-
-app.post("/add_post", async (req, res) => {
-    await Post.create(req.body)
-    res.redirect("/")
-})
+app.get("/", postController.getAllPosts)
+app.get("/posts/:id", postController.getPost)
+app.post("/add_post", postController.createPost)
+app.put("/posts/:id", postController.updatePost)
+app.delete("/posts/:id", postController.deletePost)
 
 
 
-
+app.get("/about", pageController.getAboutPage)
+app.get("/add_post", pageController.getAddPage)
+app.get("/posts/edit/:id", pageController.getEditPage)
 
 
 
